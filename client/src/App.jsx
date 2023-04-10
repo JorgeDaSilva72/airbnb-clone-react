a8c36fafc8f6340fc5d64a5987988c73c13af4fe
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2023-04-10 13:53:53 +0200

    Création d'un Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
+import Layout from "./Layout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
       </Routes>
     </>
   );
